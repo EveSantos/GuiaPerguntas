c05da191eb9aeab4f5b43f06726a728f2a0048fa
Evelyn dos Santos <evelynsantos@ibm.com>
2022-07-21 16:17:17 -0300

    : hammer: Criando e configurando rota post para pegar os parametros :hammer:

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Definindo para o Express que usará o ejs como View Engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+// Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Rotas
 app.get("/", function(req,res){
     res.render("index.ejs");
 });
 
 app.get("/perguntar", function(req,res){
     res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res)=>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Form recebido! titulo " + titulo + " " + "descricao " + descricao);
 });
 
 app.listen(8080, ()=>{
